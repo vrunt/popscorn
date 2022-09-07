86c0c0fc453ad7a31bc3ee1ea751406b1790781d
vrunt <hiimvrunt@gmail.com>
2022-09-07 09:57:51 -0400

    first commit

---
+++ routes/index.tsx
@@ -0,0 +1,19 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "@twind";
+import Counter from "../islands/Counter.tsx";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+
+const { APPLICATION_NAME } = config()
+
+export default function Home() {
+  return (
+    <div class={tw`p-4 mx-auto max-w-screen-md`}>
+      <h1 class={tw`text-9xl`}>🍿🍿🍿🍿🍿</h1>
+      <p class={tw`my-6`}>
+        Welcome to {APPLICATION_NAME}.
+      </p>
+      <Counter start={3} />
+    </div>
+  );
+}
