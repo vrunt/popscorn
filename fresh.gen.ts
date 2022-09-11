2f4ed1d3ae8ed3c76ac23e46f68fd03b51a7e0b2
vrunt <hiimvrunt@gmail.com>
2022-09-11 18:52:23 -0400

    removed some chaff, built out auth folder

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,15 +4,19 @@
 
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_middleware.tsx";
-import * as $1 from "./routes/index.tsx";
-import * as $2 from "./routes/m/[movie].tsx";
+import * as $1 from "./routes/auth/[provider]/callback.tsx";
+import * as $2 from "./routes/auth/[provider]/index.tsx";
+import * as $3 from "./routes/index.tsx";
+import * as $4 from "./routes/m/[movie].tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
     "./routes/_middleware.tsx": $0,
-    "./routes/index.tsx": $1,
-    "./routes/m/[movie].tsx": $2,
+    "./routes/auth/[provider]/callback.tsx": $1,
+    "./routes/auth/[provider]/index.tsx": $2,
+    "./routes/index.tsx": $3,
+    "./routes/m/[movie].tsx": $4,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
